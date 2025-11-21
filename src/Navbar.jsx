4682dca2f37650e949be1bc088c422a4760fd200
https://github.com/ShindePratikshaP <shindepratikshapratap9@gmail.com>
2025-11-21 14:19:54 +0530

    new changes are added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,15 @@
+
 import "./Navbar.css";
+
+import { Link } from "react-router";
 
 function Navbar({activePage}) {
   return (
     
         <div className="navbar">
-          <a href="/" className={`page ${activePage === "/" ? "page-active" : ""}`}>Home</a>
-          <a href="/About" className={`page ${activePage === "/About" ? "page-active" : ""}`}>About</a>
-          <a href="/contact" className={`page ${activePage === "/contact" ? "page-active" : ""}`}>Contact</a>
+          <Link to="/" className={`page ${activePage === "/" ? "page-active" : ""}`}>Home</Link>
+          <Link to="/About" className={`page ${activePage === "/About" ? "page-active" : ""}`}>About</Link>
+          <Link to="/contact" className={`page ${activePage === "/contact" ? "page-active" : ""}`}>Contact</Link>
         </div>
   )
 }
